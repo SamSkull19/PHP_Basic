f129673d9cd63c3077d5a780bf064fbe005e67be
sifat-samin <sifatsamin19@gmail.com>
2025-10-26 17:21:01 +0600

    PHP Increment & Decrement

---
+++ IncrementDecrement.php
@@ -0,0 +1,14 @@
+<?php
+$x = 5;
+
+echo $x++; // post-increment
+echo "<br>";
+
+echo ++$x; // pre-increment
+echo "<br>";
+
+echo $x--; // post-decrement
+echo "<br>";
+
+echo --$x; // pre-decrement
+?>
