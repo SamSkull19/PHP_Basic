07b8435e8fe5fbe8a34afba6523c0c41e0f85db7
sifat-samin <sifatsamin19@gmail.com>
2025-10-26 17:12:57 +0600

    PHP Arithmetic Operators

---
+++ ArithmeticOperators.php
@@ -0,0 +1,50 @@
+<?php
+// Arithmetic operators
+$a = 10;
+$b = 5;
+
+echo $a + $b; // addition
+echo "<br>";
+
+echo $a - $b; // subtraction
+echo "<br>";
+
+echo $a * $b; // multiplication
+echo "<br>";
+
+echo $a / $b; // division
+echo "<br>";
+
+echo $a % $b; // modulus
+echo "<br>";
+
+echo $a ** $b; // exponentiation
+echo "<br><br>";
+
+// Assignment operators
+$c = 20;
+
+$c += 5; 
+echo $c;
+echo "<br>";
+
+$c -= 3;
+echo $c;
+echo "<br>";
+
+$c *= 2;
+echo $c;
+echo "<br>";
+
+$c /= 4;
+echo $c;
+echo "<br>";
+
+$c %= 3;
+echo $c;
+echo "<br>";
+
+$c **= 2;
+echo $c;
+echo "<br>";
+?>
