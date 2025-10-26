fceb32d65eccf08f49159772c08146edf6f7bb54
sifat-samin <sifatsamin19@gmail.com>
2025-10-26 15:55:22 +0600

    PHP Basic Syntax

---
+++ BasicSyntax.php
@@ -0,0 +1,24 @@
+<?php
+echo 'Sifat Samin';
+echo '<br>';
+echo 'PHP My Admin';
+echo '<br>';
+echo 9 + 10;
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo 'My Document'; ?></title>
+</head>
+<body>
+    <p>THis is inside HTML</p>
+</body>
+</html>
+
+
+<?php
+print "This is Done by PHP print"; ?>
