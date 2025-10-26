6d59661fa35e486fe2083a9cdc75d8a6638859fb
sifat-samin <sifatsamin19@gmail.com>
2025-10-26 16:40:00 +0600

    PHP Single Vs Double Quotes

---
+++ SingleVsDoubleQuotes.php
@@ -0,0 +1,17 @@
+<?php
+$name = "Samin";
+
+echo 'Hello $name';       // outputs: Hello $name
+echo '<br>';
+echo 'It\'s a nice day'; // use backslash to escape single quote
+
+echo "<br>";
+
+$name = "Samin";
+
+echo "Hello $name";      // outputs: Hello Samin
+echo "<br>";
+echo "This is line 1\nThis is line 2"; // newline works in browser source (use <br> for HTML)
+echo "<br>";
+echo "She said, \"Hi!\""; // double quotes inside string
+?>
