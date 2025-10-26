1306adaf9400df419122cd3ba50d3fff3889927b
sifat-samin <sifatsamin19@gmail.com>
2025-10-26 17:08:17 +0600

    PHP DataTypes

---
+++ DataTypes.php
@@ -0,0 +1,50 @@
+<?php
+    $num = 4;
+    echo $num;
+    echo "<br/>";
+
+    var_dump($num);
+    echo "<br/>";
+
+    echo gettype($num);
+    echo "<br/><br/>";
+
+    $float = 4.54;
+    echo $float;
+
+    echo "<br/>";
+
+    var_dump($float);
+    echo "<br/>";
+
+    echo gettype($float);
+    echo "<br/><br/>";
+
+
+    $isStudent = true;
+    echo $isStudent; 
+    echo "<br/>";
+
+    var_dump($isStudent);
+    echo "<br/>";
+
+    echo gettype($isStudent);
+    echo "<br/><br/>";
+
+    $name = "Sifat";
+    echo $name;
+    echo "<br/>";
+
+    var_dump($name);
+    echo "<br/>";
+    
+    echo gettype($name);
+    echo "<br/><br/>";
+
+    $nulls = null;
+    echo $nulls;
+    echo "<br/>";
+
+    var_dump($nulls);
+    echo "<br/>";
+?>
