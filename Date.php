f49745651335d63f4569d652ec5475948220e914
sifat-samin <sifatsamin19@gmail.com>
2025-10-26 23:56:22 +0600

    Date

---
+++ Date.php
@@ -0,0 +1,22 @@
+<?php
+    echo "Day : " . date('d');
+    echo "<br>";
+
+    echo "Month : " . date('m');
+    echo "<br>";
+
+    echo "Year : " . date('y');
+    echo "<br>";
+
+    echo date('d | m | Y');
+    echo "<br>";
+
+    echo date('d / m / Y');
+    echo "<br>";
+
+    echo date('Y / m / d / l');
+    echo "<br>";
+    
+    echo date('Y - m - d - l');
+    echo "<br>";
+?>
