e7759b66e1378e94f1278426b66c10fa2beee710
sifat-samin <sifatsamin19@gmail.com>
2025-10-26 17:18:37 +0600

    PHP Comparison Operators

---
+++ ComparisonOperators.php
@@ -0,0 +1,50 @@
+<?php
+$a = 10;
+$b = 20;
+
+echo ($a == $b); // equal
+echo "<br>";
+
+echo ($a != $b); // not equal
+echo "<br>";
+
+echo ($a > $b); // greater than
+echo "<br>";
+
+echo ($a < $b); // less than
+echo "<br>";
+
+echo ($a >= $b); // greater than or equal
+echo "<br>";
+
+echo ($a <= $b); // less than or equal
+echo "<br>";
+
+echo ($a <=> $b); // spaceship operator
+?>
+
+<?php
+$a = 10;
+$b = 20;
+
+var_dump($a == $b);
+echo "<br>";
+
+var_dump($a != $b);
+echo "<br>";
+
+var_dump($a > $b);
+echo "<br>";
+
+var_dump($a < $b);
+echo "<br>";
+
+var_dump($a >= $b);
+echo "<br>";
+
+var_dump($a <= $b);
+echo "<br>";
+
+var_dump($a <=> $b);
+?>
+
