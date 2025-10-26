4581ba14e2f0ece557b2753719455f7c3a1e7077
sifat-samin <sifatsamin19@gmail.com>
2025-10-26 16:13:27 +0600

    PHP Variables

---
+++ Variables.php
@@ -0,0 +1,32 @@
+<?php
+$username = 'Samin';
+echo $username;
+
+echo '<br>';
+
+$a = 207; 
+$b = 54;
+echo $a + $b;
+
+echo '<br>';
+
+$firstName = "Sifat";
+$lastName = "Sarker";
+echo "Full Name: " . $firstName . " " . $lastName;
+
+echo '<br>';
+
+$age = 22;
+$height = 5.9;
+echo "Age: $age, Height: $height ft";
+
+echo '<br>';
+
+$isStudent = true;
+echo "Is Student: " . ($isStudent ? "Yes" : "No");
+
+echo '<br>';
+
+$language = "PHP";
+echo "I am learning $language programming!";
+?>
